42d0d1534b0f3244b9c56bd16ba44d10712e3b9c
Chavez Harris <codedbychavez@gmail.com>
2024-03-11 22:24:54 -0300

    complete fe

--- elixchat/assets/js/user_socket.js
+++ elixchat/assets/js/user_socket.js
@@ -1,65 +1,43 @@
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "assets/js/app.js".
-
-// Bring in Phoenix channels client library:
 import { Socket } from "phoenix"
 
-// And connect to the path in "lib/elixchat_web/endpoint.ex". We pass the
-// token for authentication. Read below how it should be used.
 let socket = new Socket("/socket")
 
-// When you connect, you'll often need to authenticate the client.
-// For example, imagine you have an authentication plug, `MyAuth`,
-// which authenticates the session and assigns a `:current_user`.
-// If the current user exists you can assign the user's token in
-// the connection for use in the layout.
-//
-// In your "lib/elixchat_web/router.ex":
-//
-//     pipeline :browser do
-//       ...
-//       plug MyAuth
-//       plug :put_user_token
-//     end
-//
-//     defp put_user_token(conn, _) do
-//       if current_user = conn.assigns[:current_user] do
-//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-//         assign(conn, :user_token, token)
-//       else
-//         conn
-//       end
-//     end
-//
-// Now you need to pass this token to JavaScript. You can do so
-// inside a script tag in "lib/elixchat_web/templates/layout/app.html.heex":
-//
-//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-//
-// You will need to verify the user token in the "connect/3" function
-// in "lib/elixchat_web/channels/user_socket.ex":
-//
-//     def connect(%{"token" => token}, socket, _connect_info) do
-//       # max_age: 1209600 is equivalent to two weeks in seconds
-//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1_209_600) do
-//         {:ok, user_id} ->
-//           {:ok, assign(socket, :user, user_id)}
-//
-//         {:error, reason} ->
-//           :error
-//       end
-//     end
-//
-// Finally, connect to the socket:
+let list = $('#messages');
+
+// Connect to the socket:
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic.
-// Let's assume you have a channel with a topic named `room` and the
-// subtopic is its id - in this case 42:
+// Set the channel
 let channel = socket.channel("chat_room:lobby", {})
 
 channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("ok", resp => {
+    console.log("Joined successfully", resp);
+
+    // Send a message to request messages after joining
+    channel.push("get_messages_after_join", {});
+
+    // Handle messages received after joining
+    channel.on("messages", payload => {
+      for (let i = 0; i < payload.messages.length; i++) {
+        let message = payload.messages[i];
+        list.append(`
+        <div class="message-card">
+        <div class="left-col">
+          <b>${message.sender}</b>
+          <p class="message-preview">
+            ${message.text}
+          </p>
+        </div>
+        <div class="right-col">
+          ${payload.message_cta_text}
+        </div>
+      </div>
+        `)
+      }
+    })
+
+  })
   .receive("error", resp => { console.log("Unable to join", resp) })
 
 export default socket
--- elixchat/lib/elixchat_web/channels/chat_room_channel.ex
+++ elixchat/lib/elixchat_web/channels/chat_room_channel.ex
@@ -1,3 +1,14 @@
+defmodule Message do
+  defstruct id: 0, sender: "", text: ""
+
+  # Implement Jason.Encoder for the Message struct
+  defimpl Jason.Encoder, for: __MODULE__ do
+    def encode(message, _opts) do
+      Map.from_struct(message) |> Jason.encode!()
+    end
+  end
+end
+
 defmodule ElixchatWeb.ChatRoomChannel do
   use ElixchatWeb, :channel
 
@@ -13,8 +24,9 @@ defmodule ElixchatWeb.ChatRoomChannel do
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
   @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_in("get_messages_after_join", _payload, socket) do
+    # Respond with the list of messages when the client requests it
+    {:reply, {:ok, socket |> push_messages()}, socket}
   end
 
   # It is also common to receive messages from the client and
@@ -23,6 +35,29 @@ defmodule ElixchatWeb.ChatRoomChannel do
   def handle_in("shout", payload, socket) do
     broadcast(socket, "shout", payload)
     {:noreply, socket}
+  end
+
+  defp push_messages(socket) do
+    # Determine the feature flag value dynamically
+    # feature_flag_value = MyApp.FeatureFlag.enabled?(:my_feature)
+
+    # Set the variable and create a message to send to the client
+    # variable_value = if feature_flag_value, do: "Enabled", else: "Disabled"
+    message_cta_text = "Read Now"
+
+    # Create a message to send to the client
+    message = %{
+      event: "messages",
+      messages: [
+        %Message{id: 1, sender: "Joe", text: "Hi, this is Joe, please call me. Thanks"},
+        %Message{id: 2, sender: "Suzan", text: "Suzan here, When should we start the meeting?"},
+        %Message{id: 3, sender: "Ann", text: "Its Ann, 10AM appointment still on."}
+      ],
+      message_cta_text: message_cta_text
+    }
+
+    # Push messages to the client
+    push(socket, "messages", message)
   end
 
   # Add authorization logic here as required.
